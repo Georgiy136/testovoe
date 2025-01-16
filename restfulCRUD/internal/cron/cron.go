8b8c4c8c3dc1198ca6154b6b7e25d390aae194d0
Бароян Гиоргий <giorgiy_03@mail.ru>
2025-01-16 22:49:51 +0300

    fix

--- restfulCRUD/internal/cron/cron.go
+++ restfulCRUD/internal/cron/cron.go
@@ -1,24 +1,63 @@
 package cron
 
-import "myapp/clients"
+import (
+	"fmt"
+	"log"
+	"myapp/clients"
+	"myapp/internal/usecase/repository"
+	"myapp/pkg/cache"
+	"strings"
+	"sync"
+	"time"
+)
 
-func NewCron(binanceApiClient clients.BinanceApiClient, secondsInterval int64) *Cron {
+func NewCron(binanceApiClient clients.BinanceApiClient, repo repository.Project, cache cache.Cache, secondsInterval int) *Cron {
 	return &Cron{
 		binanceApiClient: binanceApiClient,
 		secondsInterval:  secondsInterval,
+		cache:            cache,
+		repo:             repo,
 	}
 }
 
 type Cron struct {
 	binanceApiClient clients.BinanceApiClient
-	secondsInterval  int64
+	secondsInterval  int
+	cache            cache.Cache
+	repo             repository.Project
 }
 
 func (c *Cron) Configure() {
+	// получить основные койны из БД
+
+	// установливаем в кэш
+
 }
 
 func (c *Cron) Work() {
 	for {
+		errMsg := strings.Builder{}
+		wg := &sync.WaitGroup{}
+		// получаем названия койнов из памяти
+		for coin := range c.cache.GetListCoins() {
+			wg.Add(1)
+			// выполняем запрос http и получаем актуальную инфо-ию
+			go func(coin string) {
+				defer wg.Done()
+				res, err := c.binanceApiClient.GetCoin(coin)
+				if err != nil {
+					errMsg.WriteString(fmt.Sprintf("[ERROR] Get coin %s error: %s\n", coin, err.Error()))
+				}
+				// запись инфо в БД
 
+			}(coin)
+		}
+
+		wg.Wait()
+
+		if errMsg.Len() > 0 {
+			log.Println(errMsg.String())
+		}
+		time.Sleep(time.Second * c.secondsInterval)
 	}
 }
--- restfulCRUD/pkg/cache/storage.go
+++ restfulCRUD/pkg/cache/storage.go
@@ -1,1 +1,43 @@
 package cache
+
+import (
+	"sync"
+)
+
+type Cache struct {
+	coins map[string]struct{}
+
+	mu sync.RWMutex
+}
+
+func NewCache() *Cache {
+	return &Cache{
+		coins: make(map[string]struct{}),
+	}
+}
+
+func (c *Cache) GetListCoins() map[string]struct{} {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	return c.coins
+}
+
+func (c *Cache) AddCoins(coins []string) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	for _, coin := range coins {
+		c.coins[coin] = struct{}{}
+	}
+}
+
+func (c *Cache) AddCoin(coin string) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.coins[coin] = struct{}{}
+}
+
+func (c *Cache) DeleteCoin(coin string) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	delete(c.coins, coin)
+}
